4f234cc9185469c2ace1552d550e7477e1521845
rishiraj007 <rishiraj@mounttalent.com>
2023-06-30 16:38:10 +0530

    [Prabhu]Add: Ability to add a new Contact to Address Book

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -6,6 +6,25 @@
         {
             Console.WriteLine("Address Book System!");
             Contact contact = new Contact();
+            AddressBookMain addressBookMain = new AddressBookMain();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter your choice \n1.Add Contacts \n2.View Contacts \n3.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addressBookMain.AddContact();
+                        break;
+                    case 2:
+                        addressBookMain.ViewDetail();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
