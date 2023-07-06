2f84824f750817cf31a4ec0886f8bad28673c754
rishiraj007 <rishiraj@mounttalent.com>
2023-07-06 18:37:18 +0530

    [Prabhu]Add: Ability to Read or Write the Address Book with Persons Contact into a File using File IO

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,16 +4,19 @@
     {
         static void Main(string[] args)
         {
+            string personsContact = @"C:\Users\91789\Desktop\New folder\Bridgelabz\AddressBook\AddressBookSystem\PersonsContact.txt";
             Console.WriteLine("Address Book System!");
             Contact contact = new Contact();
             AddressBookMain addressBookMain = new AddressBookMain();
+            PersonsContactUsingFileIO fileIO = new PersonsContactUsingFileIO();
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("\nEnter your choice \n1.Add Contacts \n2.View Contacts \n3.Edit Contacts \n4.Delete Contacts " +
                     "\n5.Add Multiple Person \n6.Add Multiple AddressBook using Dictionary \n7.Display List of Dictionary " +
                     "\n8.Check for Duplicate Contacts \n9.Searc person by city or state \n10.View person by city or state " +
-                    "\n11.Count person by city or state \n12.Sort contacts by name \n13.Sort entries by city,state or zip code \n14.Exit");
+                    "\n11.Count person by city or state \n12.Sort contacts by name \n13.Sort entries by city,state or zip code " +
+                    "\n14.File Exit or Not \n15.Read Person File \n16.Write Person File \n17.ExitExit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -55,6 +58,15 @@
                         break;
                     case 13:
                         addressBookMain.SortContactByCity_State_ZipCode();
+                        break;
+                    case 14:
+                        fileIO.CheckPersonsContactsFileExists(personsContact);
+                        break;
+                    case 15:
+                        fileIO.ReaderPersonsContactsFile(personsContact);
+                        break;
+                    case 16:
+                        fileIO.WriterPersonsContactsFile(personsContact);
                         break;
                     default:
                         flag = false;
